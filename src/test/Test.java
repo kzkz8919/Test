14b36fb9b683bf46c9ee80a9794621e5ab1fa46d
kzkz8919 <kzkz8919@126.com>
2017-10-30 20:59:15 +0800

    test

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+
+	public void doSome() {
+		int a = 1;
+		int b = 2;
+		int sum = a + b;
+		System.out.print(sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("");
+	}
+
+}
